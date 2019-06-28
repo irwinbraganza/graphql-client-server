c039af8c3e27bbb435dd7a48e10605e38ce5a5c1
Irwin Braganza <irwin.braganza@honestfoodcompany.de>
2019-06-28 13:42:59 +0530

    adding models for diff schemas

--- server/schema.ts
+++ server/schema.ts
@@ -1,8 +1,8 @@
 import { merge } from 'lodash';
 
-import {typeDef as User, resolvers as userResolvers} from './schemas/users';
-import {typeDef as Role, resolvers as roleResolvers} from './schemas/roles';
-import {typeDef as Hello, resolvers as helloResolvers} from './schemas/hello';
+import {typeDef as User, resolvers as userResolvers} from './models/users';
+import {typeDef as Role, resolvers as roleResolvers} from './models/roles';
+import {typeDef as Hello, resolvers as helloResolvers} from './models/hello';
 
 const Query = `
   type Query {
